ecedd6a28bae90ebaede29973c0828b489c6108f
Jack Spira <jjspira@gmail.com>
2019-11-04 17:02:35 -0500

    Reduced further for fun

--- src/component.rs
+++
@@ -1,5 +0,0 @@
-#[derive(Debug, Clone, Default)]
-pub struct Component<T> {
-    pub is_active: bool,
-    inner: T,
-}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,32 @@
 #![warn(elided_lifetimes_in_paths)]
 
-mod component;
-mod entities;
-
-pub use component::Component;
-pub use entities::*;
-
 fn main() {
     let list: ComponentList<f32> = unimplemented!();
-    
-    for this_member in list.iter() {
 
+    for this_member in list.iter() {}
+}
+
+struct ArrayEntry<T> {
+    value: T,
+}
+
+// An array from GenerationalIndex to some Value T.
+pub struct GenerationalIndexArray<T>(Vec<Option<ArrayEntry<T>>>);
+
+impl<T> GenerationalIndexArray<T> {
+    pub fn iter_mut(&mut self) -> impl Iterator<Item = &mut T> {
+        self.0
+            .iter_mut()
+            .enumerate()
+            .flat_map(|opt_ent| Some(&mut opt_ent.1.as_mut()?.value))
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = &T> {
+        self.0
+            .iter()
+            .enumerate()
+            .flat_map(|opt_ent| Some(&opt_ent.1.as_ref()?.value))
     }
 }
+
+pub type ComponentList<T> = GenerationalIndexArray<T>;
